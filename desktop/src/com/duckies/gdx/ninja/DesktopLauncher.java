472f60765f8d1581ece63afa28a0376d1f323234
Francois Melkonian <francois.melkonian@gmail.com>
2023-03-10 00:04:25 +0100

    Initial commit

---
+++ desktop/src/com/duckies/gdx/ninja/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.duckies.gdx.ninja;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.duckies.gdx.ninja.NinjaGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Ninja");
+		new Lwjgl3Application(new NinjaGame(), config);
+	}
+}
